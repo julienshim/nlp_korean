23b375085e53d8408fede618222086908c9814b3
julienshim <25723428+julienshim@users.noreply.github.com>
2023-01-14 19:18:05 -0800

    expanded pos kor han

--- server/middlewares/paginatedResults.js
+++ server/middlewares/paginatedResults.js
@@ -4,6 +4,9 @@ module.exports = (model) => {
   return async (req, res, next) => {
     const page = parseInt(req.query.page, 10);
     const limit = parseInt(req.query.limit, 10);
+    const pos = req.query.pos;
+    const kor = req.query.kor;
+    const han = req.query.han;
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
@@ -26,12 +29,21 @@ module.exports = (model) => {
       };
     }
 
-    results.total_companies = entriesCount.rows[0].count;
+    results.total_entries = entriesCount.rows[0].count;
 
     try {
-      const allEntries = await pool.query(
-        `SELECT * FROM ${model} ORDER BY id LIMIT ${limit} OFFSET ${startIndex}`
-      );
+      let query = "";
+      console.log(han, han === undefined);
+      if (han === undefined) {
+        query = `SELECT * FROM ${model} WHERE korean='${kor}' AND part_of_speech='${pos}' ORDER BY CASE WHEN vocabulary_by_level = '초급' THEN 1 WHEN vocabulary_by_level = '중급' THEN 2 WHEN vocabulary_by_level ='고급' THEN 3 ELSE 4 END LIMIT ${limit} OFFSET ${startIndex}`;
+      } else {
+        if (han.startsWith("%") || han.endsWith("%")) {
+          query = `SELECT * FROM ${model} WHERE korean='${kor}' AND part_of_speech='${pos}' AND original_language LIKE '${han}' ORDER BY CASE WHEN vocabulary_by_level = '초급' THEN 1 WHEN vocabulary_by_level = '중급' THEN 2 WHEN vocabulary_by_level ='고급' THEN 3 ELSE 4 END LIMIT ${limit} OFFSET ${startIndex}`;
+        } else {
+          query = `SELECT * FROM ${model} WHERE korean='${kor}' AND part_of_speech='${pos}' AND original_language='${han}' ORDER BY CASE WHEN vocabulary_by_level = '초급' THEN 1 WHEN vocabulary_by_level = '중급' THEN 2 WHEN vocabulary_by_level ='고급' THEN 3 ELSE 4 END LIMIT ${limit} OFFSET ${startIndex}`;
+        }
+      }
+      const allEntries = await pool.query(query);
       results.results = allEntries.rows;
       res.paginatedResults = results;
       next();
