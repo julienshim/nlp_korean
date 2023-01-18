36efb6e648010aaf4456289d7aa73d7a005f118f
julienshim <25723428+julienshim@users.noreply.github.com>
2023-01-17 19:21:15 -0800

    pos.py init

---
+++ reference/pos.py
@@ -0,0 +1,127 @@
+_korean_pos = {
+    "명사": {
+        "pos_english": "noun",
+        "tags": {
+            "NNG": "보통명사",
+            "NNP": "고유명사"
+        },
+        "vocab_list_pos": '명사'
+    },
+    # "접사": {
+    #     "pos_english": "affix",
+    #     "tags": {
+    #         "XPN": "체언 접두사",
+    #         "XPV": "용언 접두사",
+    #         "XSN": "명사파생 접미사",
+    #         "XSV": "동사 파생 접미사",
+    #         "XSA": "형용사 파생 접미사",
+    #         "XR": "어근"
+    #     }
+    # },
+    # "조사": {
+    #     "pos_english": "postpositional particle",
+    #     "tags": {
+    #         "JKS": "주격 조사",
+    #         "JKC": "보격 조사",
+    #         "JKG": "관형격 조사",
+    #         "JKO": "목적격 조사",
+    #         "JKM": "부사격 조사",
+    #         "JKI": "호격 조사",
+    #         "JKQ": "인용격 조사",
+    #         "JC": "접속 조사",
+    #         "JX": "보조사"
+    #     }
+    # },
+    "부사": {
+        "pos_english": "adverb",
+        "tags": {
+            "MAG": "일반 부사",
+            "MAC": "접속 부사"
+        },
+        "vocab_list_pos": "부사"
+    },
+    "동사": {
+        "pos_english": "verb",
+        "tags": {
+            "VV": "동사"
+        },
+        "vocab_list_pos": "동사"
+    },
+    "관형사": {
+        "pos_english": "determiner",
+        "tags": {
+            "MDN": "수 관형사",
+            "MDT": "일반 관형사"
+        },
+        "vocab_list_pos": "관형사"
+    },
+     "형용사": {
+        "pos_english": "adjective",
+        "tags": {
+            "VA": "형용사"
+        },
+        "vocab_list_pos": "형용사"
+    },
+    # "보조 동사": {
+    #     "pos_english": "auxiliary verb",
+    #     "tags": {
+    #         "VXV": "보조 동사"
+    #     },
+    #     "vocab_list_pos": "보조 용언"
+    # },
+    "감탄사": {
+        "pos_english": "interjection",
+        "tags": {
+            "IC": "감탄사"
+        },
+        "vocab_list_pos": "감탄사"
+    },
+    # "의존 명사": {
+    #     "pos_english": "bound noun",
+    #     "tags": {
+    #         "NNB": "일반 의존 명사",
+    #         "NNM": "단위 의존 명사"
+    #     },
+    #     "vocab_list_pos": "의존 명사"
+    # },
+    "대명사": {
+        "pos_english": "pronoun",
+        "tags": {
+            "NP": "대명사"
+        },
+        "vocab_list_pos": "대명사"
+    },
+    # "어미": {
+    #     "pos_english": "ending word",
+    #     "tags": {
+    #         "EPH": "존칭 선어말 어미",
+    #         "EPT": "시제 선어말 어미",
+    #         "EPP": "공손 선어말 어미",
+    #         "EFN": "평서형 종결 어미",
+    #         "EFQ": "의문형 종결 어미",
+    #         "EFO": "명령형 종결 어미",
+    #         "EFA": "청유형 종결 어미",
+    #         "EFI": "감탄형 종결 어미",
+    #         "EFR": "존칭형 종결 어미",
+    #         "ECE": "대등 연결 어미",
+    #         "ECS": "보조적 연결 어미",
+    #         "ECD": "의존적 연결 어미",
+    #         "ETN": "명사형 전성 어미",
+    #         "ETD": "관형형 전성 어미"
+    #     }
+    # },
+    "수사": {
+        "pos_english": "numeral",
+        "tags": {
+            "NR": "수사"
+        },
+        "vocab_list_pos": "수사"
+    },
+    # "보조 형용사": {
+    #     "pos_english": "auxiliary adjective",
+    #     "tags": {
+    #         "VXA": "보조 형용사"
+    #     },
+    #     "vocab_list_pos": "보조 용언"
+    # }    
+}
