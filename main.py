d6153699e0915d539fb35e32b557ef88290be2b9
julienshim <25723428+julienshim@users.noreply.github.com>
2023-01-19 20:48:41 -0800

    gen vocab a txt

--- main.py
+++ main.py
@@ -9,4 +9,10 @@ target_pdfs = '<target_files_location>'
     print('\t'.join(['RUNNING:', cmd]))
     system(cmd)
 
-
+# Generate Vocabulary Lists for From PDF Converted TXT
+target_txts = list(map(lambda x: x.replace('.pdf', '.txt'), target_pdfs))
+for txt_filename in target_txts[0:2]:
+    txt_path = path.join('<target_files_location>', txt_filename)
+    cmd = f'python3 ./src/generate_vocab_csv_from_txt.py {txt_path}'
+    print('\t'.join(['RUNNING:', cmd]))
+    system(cmd)
